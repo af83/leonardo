517570b64acf2611b1e83fad85043ca057f2812a
ophir <ophir.lojkine@af83.com>
2014-08-01 17:21:52 +0200

    Javascriptify the code

--- leonardo.js
+++ leonardo.js
@@ -1,10 +1,24 @@
 (function() {
-  this.App.Model.filter = {
-    applyFilter: function(image, filter) {
-      var ctx, cv, filtered, fun, imageData;
-      cv = document.createElement("canvas");
-      cv.setAttribute("width", image.width);
-      cv.setAttribute("height", image.height);
+  leonardo = {
+
+    createCanvas: function createCanvas(width, height) {
+      var canvas;
+      canvas = document.createElement('canvas');
+      canvas.width = width;
+      canvas.height = height;
+      return canvas;
+    }
+
+    "applyFilter": function(image, filterName) {
+      var ctx, cv, filter, filtered, fun, imageData;
+      for (var i=0, l = this.filters.length; i<l; ++i) {
+        if (this.filters[i].name === filterName) {
+          filter = this.filters[i];
+          break;
+        }
+      }
+      if (!filter) throw "No such filter: " + filterName;
+      cv = this.createCanvas(image.width, image.height);
       ctx = cv.getContext('2d');
       ctx.drawImage(image, 0, 0);
       imageData = ctx.getImageData(0, 0, image.width, image.height);
@@ -18,31 +32,34 @@
       ctx.putImageData(filtered, 0, 0);
       return cv;
     },
+
     filterFromMatrix: function(m, pixels) {
-      var a, b, d, g, i, r, _i, _ref;
-      d = pixels.data;
-      for (i = _i = 0, _ref = d.length; _i < _ref; i = _i += 4) {
+      var a, b, g, i, r, d = pixels.data;
+      for (i = 0, len = d.length; i < len; i += 4) {
         r = m[0][0] * d[i] + m[0][1] * d[i + 1] + m[0][2] * d[i + 2] + m[0][3] * d[i + 3] + m[0][4] * 255;
         g = m[1][0] * d[i] + m[1][1] * d[i + 1] + m[1][2] * d[i + 2] + m[1][3] * d[i + 3] + m[1][4] * 255;
         b = m[2][0] * d[i] + m[2][1] * d[i + 1] + m[2][2] * d[i + 2] + m[2][3] * d[i + 3] + m[2][4] * 255;
+
         if (m[3]) {
           a = m[3][0] * d[i] + m[3][1] * d[i + 1] + m[3][2] * d[i + 2] + m[3][3] * d[i + 3] + m[3][4] * 255;
-        }
-        d[i] = r;
-        d[i + 1] = g;
-        d[i + 2] = b;
-        if (a != null) {
           d[i + 3] = a;
         }
+
+        d[i    ] = r;
+        d[i + 1] = g;
+        d[i + 2] = b;
       }
       return pixels;
     },
+
     functions: {
       popart: function(pixels) {
-        var d, h, i, j, orig, q, w, x, y, _i, _ref, _ref1;
-        _ref = [pixels.data, pixels.width, pixels.height], d = _ref[0], w = _ref[1], h = _ref[2];
-        orig = new Uint8ClampedArray(d);
-        for (i = _i = 0, _ref1 = d.length; _i < _ref1; i = _i += 4) {
+        var i, j, q, x, y,
+            d = pixels.data,
+            w = pixels.width,
+            h = pixels.height,
+            orig = new Uint8ClampedArray(d);
+        for (i = 0, len = d.length; i < len; i += 4) {
           x = (i / 4) % w;
           y = (i / 4) / w | 0;
           q = (x > w / 2) + 2 * (y > h / 2);
@@ -55,24 +72,36 @@
         return pixels;
       }
     },
+
     filters: [
       {
         name: "greyscale",
-        matrix: [[.2126, .7152, .0722, 0, 0], [.2126, .7152, .0722, 0, 0], [.2126, .7152, .0722, 0, 0]]
+        matrix: [[.2126, .7152, .0722, 0, 0],
+                 [.2126, .7152, .0722, 0, 0],
+                 [.2126, .7152, .0722, 0, 0]]
       }, {
         name: "sepia",
-        matrix: [[.393, .769, .189, 0, 0], [.349, .686, .168, 0, 0], [.272, .534, .131, 0, 0]]
+        matrix: [[.393, .769, .189, 0, 0],
+                 [.349, .686, .168, 0, 0],
+                 [.272, .534, .131, 0, 0]]
       }, {
         name: "popart"
       }, {
         name: "vivid",
-        matrix: [[1.2, -0.1, -0.1, 0, 0], [-0.1, 1.2, -0.1, 0, 0], [-0.1, -0.1, 1.2, 0, 0]]
+        matrix: [[ 1.2, -.1, -.1, 0, 0],
+                 [-0.1, 1.2, -.1, 0, 0],
+                 [-0.1, -.1, 1.2, 0, 0]]
       }, {
         name: "polaroid",
-        matrix: [[1.438, -0.122, -0.016, 0, -.03], [-0.062, 1.378, -0.016, 0, .05], [-0.062, -0.122, 1.483, 0, -.02]]
+        matrix: [[1.438, -.122, -.016, 0, -.03],
+                 [-.062, 1.378, -.016, 0,  .05],
+                 [-.062, -.122, 1.483, 0, -.02]]
       }
     ]
   };
 
-}).call(this);
+  if (typeof window !== "undefined") window['leonardo'] = leonardo;
+  if (typeof module !== "undefined") module.exports = leonardo;
+
+})();
 
